5647c58c956137a0fc798f319c1a730f70869909
shaquib khan <connectshaquib@gmail.com>
2022-08-03 02:35:26 +0530

    work in progress

---
+++ Readme.md
@@ -0,0 +1,47 @@
+## Learning Curve
+***
+
+- Learned how to implement nested dropdown using bootstrap
+
+- Learned to use `text-transform` property of `CSS`
+
+- Learned the implementation of `carousel`.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+***
+***
+## > NOTES
+- `Carousel` is used to show images in a sliding manner on the web page.
+- It can be used in different ways
+1 automatically sliding 
+2 sliding with controls
+3 `Crossfade` which means instead of sliding the image will have a `fade transiti0n`.
+4 With Captions
+5 Sliding with indicators
+6 Disable touch swapping
+
+- We can also make custom tranition on the carousel 
---
+++ js/action.js
@@ -0,0 +1,53 @@
+let valueDisplays = document.querySelectorAll(".num");
+let interval =5000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval/endValue);
+  let counter = setInterval(function () {
+    startValue +=1;
+    valueDisplay.textContent = startValue;
+    if(startValue== endValue){
+      clearInterval(counter);
+
+    }
+  }, duration)
+})
+
+
+//  Initialize Swiper 
+
+      var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        centeredSlides: false,
+        slidesPerGroupSkip: 1,
+        grabCursor: true,
+        keyboard: {
+          enabled: true,
+        },
+        breakpoints: {
+          769: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+          },
+        },
+        scrollbar: {
+          el: ".swiper-scrollbar",
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+    
+      /* Demo purposes only */
+$(".hover").mouseleave(
+  function () {
+    $(this).removeClass("hover");
+  }
+);
